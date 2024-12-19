f1afd75a15bcda8b3512d3b4a60211a5df7b7cd2
pradeeprdev <pradeeprawat1717@gmail.com>
2024-12-19 16:29:18 +0530

    fix: cors error

--- server/server.js
+++ server/server.js
@@ -9,22 +9,9 @@ const postRoutes = require('./routes/posts');
 
 const app = express();
 
-// Configure allowed origins for CORS
-const allowedOrigins = [
-  'http://localhost:5000', // if you're running the frontend locally
-  'http://13.201.89.59:5000', // Your production domain/IP for the frontend
-  'http://13.201.89.59:5173', // Your production domain/IP for the frontend
-];
-
-// Enable CORS with the above allowed origins
+// Enable CORS for all origins
 app.use(cors({
-  origin: function(origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) { // allow no-origin requests (like Postman or direct API testing)
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'), false);
-    }
-  },
+  origin: "*",  // Allows all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,  // Allow cookies to be sent with requests (if necessary)
 }));
@@ -39,7 +26,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // API routes
 app.use("/api/uploads", express.static("uploads"));
 app.use(express.json());
-
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 
@@ -53,7 +39,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(buildpath, 'index.html'));
 });
 
-// Listen on the specified port
+// Start the server
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
