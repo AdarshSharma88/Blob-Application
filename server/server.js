d5d5f26e870b12dd77016297ad548445c6650a5c
pradeeprdev <pradeeprawat1717@gmail.com>
2024-12-19 16:31:13 +0530

    fix: cors error

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Enable CORS for all origins
 app.use(cors({
-  origin: "*",  // Allows all origins
+  origin: "http://localhost:5000",  // Allows all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,  // Allow cookies to be sent with requests (if necessary)
 }));
